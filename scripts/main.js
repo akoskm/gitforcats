9431ad884914c8cd5d38f93551da485de59cbb0a
Akos Kemives <akoskm@gmail.com>
2015-08-14 07:35:04 +0200

    #12 implement toggle button for simplified view on/off

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,7 @@
 (function () {
   var simple = false;
 
-  $('#simpleon').on('click', function (e) {
-    var span = $(e.currentTarget);
-
-    e.preventDefault();
-
+  $('#simpleswitch').on('click', function (e) {
     $('div.details').slideToggle();
-
-    if (simple) {
-      span.text("Simplified view: Off");
-    } else {
-      span.text("Simplified view: On");
-    }
-
-    // not so simple
-    simple = !simple;
   });
 })();
